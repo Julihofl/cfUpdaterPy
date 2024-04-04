b78a5b851bbfaac43df6f8ef6af15ab0af908639
Julian Hoffmann <julian.hoffmann@uni-a.de>
2024-04-04 10:29:35 +0200

    improved logging

--- cfUpdater.py
+++ cfUpdater.py
@@ -1,17 +1,24 @@
 import requests
+import sys
+import datetime
+import enum
 
 g_api_key = "<YOUR_API_KEY>"
 g_zone_id = "<YOUR_ZONE_ID>"
 g_record_names = ["www.domain.com", "domain.com", "first.domain.com", "second.domain.com"]
 g_record_type = "A"
-g_content = requests.get("https://api.ipify.org").text
+g_ip_address = requests.get("https://api.ipify.org").text
 g_header = {"Authorization": "Bearer " + g_api_key,}
+
+class LogLevel(enum.Enum):
+    INFO = "info"
+    ERROR = "error"
 
 def get_public_ip():
     try:
         return requests.get("https://api.ipify.org").text
     except requests.RequestException as e:
-        print(f"Failed to get public IP: {e}")
+        write_log(LogLevel.ERROR, f"Failed to get public IP: {e}")
         return None
 
 def get_dns_record_id(record_name):
@@ -22,13 +29,13 @@ def get_dns_record_id(record_name):
             if records:
                 return records[0]["id"]
             else:
-                print("No matching DNS record found")
+                write_log(LogLevel.ERROR, "No matching DNS record found")
                 return None
         else:
-            print(f"Failed to fetch DNS records: {response.text}")
+            write_log(LogLevel.ERROR, f"Failed to fetch DNS records: {response.text}")
             return None
     except requests.RequestException as e:
-        print(f"API request failed: {e}")
+        write_log(LogLevel.ERROR, f"API request failed: {e}")
         return None
 
 def check_dns_record(record_name):
@@ -44,11 +51,11 @@ def update_dns_record():
         record_name = record_name.strip()  # Removing leading/trailing whitespace
         dns_record_ip = check_dns_record(record_name)
         
-        if dns_record_ip == g_content:
-            print(f"Info: The IP address already matches the A record for {record_name}.\n")
+        if dns_record_ip == g_ip_address:
+            write_log(LogLevel.INFO, f"The IP address {g_ip_address} already matches the A record for {record_name}.")
             continue
         elif dns_record_ip is None:
-            print(f"Error: Could not retrieve the DNS record for {record_name}.\n")
+            write_log(LogLevel.ERROR, f"Could not retrieve the DNS record for {record_name}.")
             continue
 
         record_id = get_dns_record_id(record_name)
@@ -58,7 +65,7 @@ def update_dns_record():
         data = {
             "type": g_record_type,
             "name": record_name,
-            "content": g_content,
+            "content": g_ip_address,
             "ttl": 1,
             "proxied": False
         }
@@ -66,11 +73,21 @@ def update_dns_record():
         try:
             response = requests.put(f"https://api.cloudflare.com/client/v4/zones/{g_zone_id}/dns_records/{record_id}", json=data, headers=g_header)
             if response.status_code == 200:
-                print(f"Success: DNS record for {record_name} updated successfully.\n")
+                write_log(LogLevel.INFO, f"DNS record for {record_name} updated successfully with ip {g_ip_address}.")
             else:
-                print(f"Error: Failed to update DNS record for {record_name}: {response.text}\n")
+                write_log(LogLevel.ERROR, f"Failed to update DNS record for {record_name}: {response.text}")
         except requests.RequestException as e:
-            print(f"API request failed for {record_name}: {e}\n")
+            write_log(LogLevel.ERROR, f"API request failed for {record_name}: {e}")
+
+def write_log(level, message):
+    current_time = datetime.datetime.now()
+    message = f"{current_time:%Y-%m-%d %H:%M:%S} - {message}"
+    match level:
+        case LogLevel.INFO:
+            print(message)
+        case LogLevel.ERROR:
+            print(message, file=sys.stderr)
 
 if __name__ == "__main__":
     update_dns_record()
+    print("###################################################################################################################")
